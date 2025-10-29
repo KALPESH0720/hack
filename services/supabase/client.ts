f7f2ffde85039f3f51611c12116f1e43ba94febf
KALPESH0720 <kalpeshchorlekar07@gmail.com>
2025-10-29 22:02:50 +0530

    Use variables for Supabase URL and key
    Replaced hardcoded Supabase URL and key with variables.

--- services/supabase/client.ts
+++ services/supabase/client.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fuqmkqnjjegfwqbwhilx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1cW1rcW5qamVnZndxYndoaWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMDM3MTEsImV4cCI6MjA3Njc3OTcxMX0.ouDZG3bdWD4d749QTmEOc0JvxvNMhgA0f7Svg2L2EGo';
+const supabaseUrl = supabase_url;
+const supabaseKey = supabase_anom_key;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
